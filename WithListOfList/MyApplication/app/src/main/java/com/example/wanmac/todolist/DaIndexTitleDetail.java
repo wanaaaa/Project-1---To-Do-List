08fa39c93cc514215419f8bb6c918dd8a3f40d95
wanaaaa <wanaaaa@gmail.com>
2016-07-10 01:55:33 -0400

    adf

---
+++ WithListOfList/MyApplication/app/src/main/java/com/example/wanmac/todolist/DaIndexTitleDetail.java
@@ -0,0 +1,35 @@
+package com.example.wanmac.todolist;
+
+import java.util.ArrayList;
+
+/**
+ * Created by lenoubun16 on 7/10/16.
+ */
+public class DaIndexTitleDetail {
+    private static DaIndexTitleDetail indexTitleDetail = null;
+    private static int indexTD;
+
+    private DaIndexTitleDetail() {
+        indexTD = new Integer();
+    }
+
+
+
+
+
+
+}
+
+//    private static DaListOfList listOfList = null;
+//    private static ArrayList<TitleDetail> nameTitleDetail;
+//
+//    private DaListOfList() {
+//        nameTitleDetail = new ArrayList<TitleDetail>();
+//    }
+//
+//    static DaListOfList getInstance() {
+//        if (listOfList == null) {
+//            listOfList = new DaListOfList();
+//        }
+//        return  listOfList;
+//    }
